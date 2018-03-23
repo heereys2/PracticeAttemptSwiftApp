4a1e8e30ae48ad58dc510c715bb0148a8d72abda
Stephen Heerey <stephen.heerey2@mail.dcu.ie>
2018-03-23 12:27:50 +0000

    adding small changes

--- app/src/main/java/ie/swiftapp/practiceattemptswiftapp/MainActivity.java
+++ app/src/main/java/ie/swiftapp/practiceattemptswiftapp/MainActivity.java
@@ -2,12 +2,33 @@ package ie.swiftapp.practiceattemptswiftapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    Button btn = (Button) findViewById(R.id.loginButton);
+
+    @Override
+    public void onClick(View view) {
+        
+    }
 }
+//    btn.setOnClickListener(new View.OnClickListener() {
+//        public void onClick(View v) {
+//            loginButtonOnClick(v);
+//        }
+//    });
+//
+//    public void loginButtonOnClick(View v){
+//        Intent intent = new Intent(this, MapsActivity.class);
+//        startActivity(invent);
+//    }
+//}
